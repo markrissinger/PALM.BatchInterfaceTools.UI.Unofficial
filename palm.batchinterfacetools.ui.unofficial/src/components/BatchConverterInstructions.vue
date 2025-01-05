de008666dea34387af979ce593caca0e5b871be6
markrissinger@gmail.com <markrissinger@gmail.com>
2025-01-04 20:27:55 -0500

    updated instructions

--- palm.batchinterfacetools.ui.unofficial/src/components/BatchConverterInstructions.vue
+++ palm.batchinterfacetools.ui.unofficial/src/components/BatchConverterInstructions.vue
@@ -2,17 +2,13 @@
   <p>
     <span class="h6">Instructions</span>
     <br />
-    1. Download and complete a copy of the template below.
+    1. Download and complete a copy of the template below. Template data is flattened out, so data for records such as headers will need to be repeated (a sample file is included as a demonstration). 
     <br />
-    2. Upload the completed template to the
-    <strong>Input File</strong> field.
+    2. Upload the completed template to the <strong>Input File</strong> field.
     <br />
-    3. Optionally fill out the
-    <strong>Agency</strong> and
-    <strong>Agency Business System</strong> fields. They can be used in the name of the returned file.
+    3. Optionally fill out the <strong>Agency</strong> and <strong>Agency Business System</strong> fields. They can be used in the name of the returned file.
     <br />
-    4. Select an
-    <strong>Output Format</strong>. File will return a text file based on PALM Knowledge Center specifications. Text Contents will return just the contents of the file.
+    4. Select an <strong>Output Format</strong>. File will return a text file based on PALM Knowledge Center specifications. Text Contents will return just the contents of the file.
     <br />
   </p>
 </template>
